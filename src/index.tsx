ed79ad9ececed406bd7c692361d2bad7a7735987
Rodrigo Rocha <rodrocha444@gmail.com>
2022-05-11 23:57:21 -0300

    Corrigido erros iniciais de conflito

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,10 @@
-import { render } from 'react-dom'
+import ReactDOM from "react-dom/client";
+import {App} from './App';
 
-import { App } from './App'
+const container = document.getElementById('root')!;
 
-render(<App />, document.getElementById('root'))
+// Create a root.
+const root = ReactDOM.createRoot(container);
+
+// Initial render
+root.render(<App/>);
